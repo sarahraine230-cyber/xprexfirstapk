572b0d810b75ae999deb7789c7c5b0add8ba402c
sarahraine230-cyber <sarahraine230@gmail.com>
2025-11-30 23:31:53 +0100

    Create profile_model.dart

---
+++ lib/models/profile_model.dart
@@ -0,0 +1,28 @@
+class ProfileModel {
+  final String id; // This is the 'id' (uuid) from the profiles table
+  final String authUserId;
+  final String username;
+  final String displayName;
+  final String? avatarUrl;
+  final int followersCount;
+
+  ProfileModel({
+    required this.id,
+    required this.authUserId,
+    required this.username,
+    required this.displayName,
+    this.avatarUrl,
+    this.followersCount = 0,
+  });
+
+  factory ProfileModel.fromJson(Map<String, dynamic> json) {
+    return ProfileModel(
+      id: json['id'] as String,
+      authUserId: json['auth_user_id'] as String,
+      username: json['username'] as String,
+      displayName: json['display_name'] as String,
+      avatarUrl: json['avatar_url'] as String?,
+      followersCount: json['followers_count'] as int? ?? 0,
+    );
+  }
+}
