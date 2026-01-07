067c65538d1e75665d73998968ee4847f501ee74
sarahraine230-cyber <sarahraine230@gmail.com>
2026-01-07 20:28:59 +0100

    Create 20260107000000_activate_automations.sql

---
+++ supabase/migrations/20260107000000_activate_automations.sql
@@ -0,0 +1,26 @@
+-- 1. TRIGGER FOR WELCOME EMAIL (On New User Signup)
+-- This assumes your users are automatically copied to public.profiles
+-- If they aren't, you might need to trigger on auth.users, but profiles is safer for accessing username.
+
+create or replace trigger on_profile_created
+  after insert on public.profiles
+  for each row execute function supabase_functions.http_request(
+    'https://[YOUR_PROJECT_REF].supabase.co/functions/v1/send-welcome-email',
+    'POST',
+    '{"Content-type":"application/json"}',
+    '{}',
+    '1000'
+  );
+
+-- 2. TRIGGER FOR PREMIUM GUIDE (On Upgrade)
+create or replace trigger on_premium_upgrade
+  after update on public.profiles
+  for each row
+  when (old.is_premium is distinct from true and new.is_premium = true)
+  execute function supabase_functions.http_request(
+    'https://[YOUR_PROJECT_REF].supabase.co/functions/v1/send-premium-guide',
+    'POST',
+    '{"Content-type":"application/json"}',
+    '{}',
+    '1000'
+  );
